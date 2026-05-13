2742d8135628f10377a57f814f5c65534b26ad20
stevehu <stevehu@users.noreply.github.com>
2026-05-13 19:31:26 +0000

    deploy: lightapi/light-portal-doc@25c8d4b1857c0ac6d468bcf4ef4eca220f02b38a

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-aec838b1.js',
+                path_to_root + 'searchindex-3918ae5e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
